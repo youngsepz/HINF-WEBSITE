adee9b9167b2750b61399115ec4cb4d102f7bd87
Seper Rohani <svvenfen@gmail.com>
2024-12-08 05:35:14 -0800

    shadcn navbar scaffolding

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -9,20 +9,46 @@ import {
     NavigationMenuViewport,
 } from '@/components/ui/navigation-menu'
 
-function NavigationBar() {
+export function NavigationBar() {
     return (
-        <NavigationMenu>
+        <NavigationMenu
+            className="flex h-14 items-center px-4"
+            orientation="horizontal"
+        >
             <NavigationMenuList>
-                <NavigationMenuItem>
+                <NavigationMenuItem className="hover:bg-sky-700 hover:clr text-white-500 rounded-full">
                     <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <NavigationMenuLink>Link</NavigationMenuLink>
+                        <ul className="flex-1 grid-flow-col">
+                            <li className="w-400 h-400"></li>
+                        </ul>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
-                <NavigationMenuItem>
+
+                <NavigationMenuItem className="hover:bg-sky-700 hover:clr text-white-500 rounded-full">
                     <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <NavigationMenuLink>Link</NavigationMenuLink>
+                        <ul className="flex-1 grid-flow-col">
+                            <li className="w-400 h-400"></li>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+
+                <NavigationMenuItem className="hover:bg-sky-700 hover:clr text-white-500 rounded-full">
+                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="flex-1 grid-flow-col">
+                            <li className="w-400 h-400"></li>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+
+                <NavigationMenuItem className="hover:bg-sky-700 hover:clr text-white-500 rounded-full">
+                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="flex-1 grid-flow-col">
+                            <li className="w-400 h-400"></li>
+                        </ul>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
             </NavigationMenuList>
