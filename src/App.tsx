8de134b3cc14d2314229d072b9543d322734693a
Seper <svvenfen@gmail.com>
2024-12-09 04:19:43 -0800

    Navbar(DESKTOP) + Theme Toggle + App Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from '@/components/ui/button'
 import { NavigationBar } from './navbar'
 import { ThemeProvider } from './components/theme-provider'
+import { ModeToggle } from './components/mode-toggle'
+import UvicLogo from './assets/uvic-logo.svg'
+import UvicLogoDark from './assets/uvic-logo-dark.svg'
 
 function App() {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            {<NavigationBar />}
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+            <div className="wrapper border-b-[1px] py-3">
+                <div className="flex justify-between items-center max-w-7xl w-100 m-auto md:show max-xl:px-6">
+                    <div className="logo flex justify-center items-center text-xl">
+                        <img className="logo h-8 w-16" src={UvicLogoDark}></img>
+                        <div className="px-4">Health Information Science</div>
+                    </div>
+                    <div className="flex">
+                        <NavigationBar />
+                        <div className="px-1">
+                            <ModeToggle />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </ThemeProvider>
     )
 }
--- src/navbar.tsx
+++ src/navbar.tsx
@@ -8,54 +8,33 @@ import {
     NavigationMenuTrigger,
     NavigationMenuViewport,
 } from '@/components/ui/navigation-menu'
-import uviclogo from './assets/uvic-logo.svg'
-import { ModeToggle } from './components/mode-toggle'
 
 export function NavigationBar() {
     return (
-        <NavigationMenu
-            className="flex h-14 items-center px-4 border-b-[1px]"
-            orientation="horizontal"
-        >
-            <ModeToggle></ModeToggle>
-            <img className="w-24 h-24" src={uviclogo} alt="UVIC LOGO"></img>
+        <NavigationMenu>
             <NavigationMenuList>
-                <NavigationMenuItem class="navmenuitem">
+                <NavigationMenuItem>
                     <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <ul className="flex-1 grid-flow-col">
-                            <li className="w-400 h-400"></li>
-                        </ul>
+                        <NavigationMenuLink>Link</NavigationMenuLink>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
 
                 <NavigationMenuItem>
                     <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <ul className="flex-1 grid-flow-col">
-                            <li className="w-400 h-400"></li>
-                        </ul>
+                        <NavigationMenuLink>Link</NavigationMenuLink>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
 
                 <NavigationMenuItem>
                     <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <ul className="flex-1 grid-flow-col">
-                            <li className="w-400 h-400"></li>
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="flex-1 grid-flow-col">
-                            <li className="w-400 h-400"></li>
-                        </ul>
+                        <NavigationMenuLink>Link</NavigationMenuLink>
                     </NavigationMenuContent>
                 </NavigationMenuItem>
             </NavigationMenuList>
+            <NavigationMenuViewport />
         </NavigationMenu>
     )
 }
