f493b384cd91ef1b51ec434479e295a733a98866
Seper <svvenfen@gmail.com>
2024-12-21 03:56:32 -0800

    Fixed Timeline Bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import './App.css'
 import { Button } from '@/components/ui/button'
@@ -8,46 +8,54 @@ import { ModeToggle } from './components/mode-toggle'
 import { SideSheet } from './sidepopper'
 import Hero from './Hero'
 import { Testing } from './Test'
-import { TimelineDemo } from './Timeline';
-import {Footer} from './footer'
-
+import { TimelineDemo } from './Timeline'
+import { Footer } from './footer'
 
 function App() {
     return (
-                <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-
-                        <div className="wrapper sticky py-3 z-40">
-                        <div className="flex md:flex-row justify-between items-center max-w-7xl w-100 m-auto md:show max-xl:px-7">
-                            <div className="logo flex justify-center items-center text-xl ">
-                                <div className="font-semibold">
-                                    <a href="/">Health Information Science</a>
-                                </div>
-                            </div>
-                            <div className="flex">
-                                <div className="max-md:hidden">
-                                    <NavigationBar />
-                                </div>
-                                <div className="max-md:px-2 px-1">
-                                    <ModeToggle />
-                                </div>
-
-                                <div className="md:hidden">
-                                    <SideSheet />
-                                </div>
-                            </div>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <div className="wrapper sticky py-3 z-40">
+                <div className="flex md:flex-row justify-between items-center max-w-7xl w-100 m-auto md:show max-xl:px-7">
+                    <div className="logo flex justify-center items-center text-xl ">
+                        <div className="font-semibold">
+                            <a href="/">Health Information Science</a>
                         </div>
                     </div>
+                    <div className="flex">
+                        <div className="max-md:hidden">
+                            <NavigationBar />
+                        </div>
+                        <div className="max-md:px-2 px-1">
+                            <ModeToggle />
+                        </div>
 
-                    <Routes>
-                        <Route path="/" element={<><Hero /> <Testing/> <Hero/></>} />
-                        <Route path="/history" element={<><TimelineDemo/></>} />
-                        <Route path="/apples" element={<ModeToggle/>} />
-                    </Routes>
+                        <div className="md:hidden">
+                            <SideSheet />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-<Footer/>
-                </ThemeProvider>          
-
-
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <Hero /> <Testing /> <Hero />
+                        </>
+                    }
+                />
+                <Route
+                    path="/history"
+                    element={
+                        <>
+                            <TimelineDemo />
+                        </>
+                    }
+                />
+                <Route path="/apples" element={<ModeToggle />} />
+            </Routes>
+        </ThemeProvider>
     )
 }
 
